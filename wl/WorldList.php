a6302e402e79fd7662a9f5844fab9140b2172f90
jk <jeje9893@gmail.com>
2024-12-12 12:43:05 +0900

    nav 바 변경

--- wl/WorldList.php
+++ wl/WorldList.php
@@ -17,16 +17,25 @@ session_start();
 </head>
 
 <div>
-    <nav>
+<nav>
       <ul>
-        <li style="border-right: 1px solid black;"><a href="../WorldMain/index.php" onclick="showMode(1)">세계지도</a></li>
-        <li style="border-right: 1px solid black;"><a href="WorldList.php">국가목록</a></li>
+        <li style="border-right: 1px solid black">
+          <a href="#" onclick="showMode(1)">
+            <img src="../img/worldMap_icon.png" alt="세계지도" style="height: 35px; vertical-align: middle;">
+          </a>
+        </li>
+        <li style="border-right: 1px solid black">
+          <a href="../wl/WorldList.php">국가목록</a>
+        </li>
+        <li style="border-right: 1px solid black">
+          <a href="../phpPart/index.php">DB에 데이터 넣기</a>
+        </li>
         <?php if (isset($_SESSION['username'])): ?>
-          <li style="float: right;"><a href="#"><?php echo htmlspecialchars($_SESSION['username']); ?></a></li>
-          <li style="float: right;"><a href="../Login/logout.php">Log out</a></li>
+          <li style="float: right; border-left: 1px solid white"><a href="../Login/logout.php">Log out</a></li>
+          <li style="float: right"><a href="#"><?php echo htmlspecialchars($_SESSION['username']); ?></a></li>
         <?php else: ?>
-          <li style="float: right;"><a href="../Login/login.php">Log in</a></li>
-          <li style="float: right;"><a href="../Login/sign_up.php">Sign up</a></li>
+          <li style="float: right; border-left: 1px solid white"><a href="../Login/login.php">Log in</a></li>
+          <li style="float: right"><a href="../Login/sign_up.php">Sign up</a></li>
         <?php endif; ?>
       </ul>
     </nav>
